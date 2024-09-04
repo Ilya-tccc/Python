0e60a4fda0b76d13cef9059c40737f570a94cb71
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-09-04 13:48:37 +0300

    04/09/2024

---
+++ Lesson1.py
@@ -0,0 +1,10 @@
+#1st program
+print(9**0.5*5)
+#2st program
+print(9.99>9.98 and 1000!=1001)
+#3rd program
+print(2+2*2)
+print((2+2)*2)
+print(2+2*2<(2+2)*2)
+#4th program
+print(int(123.4568*10%10))
