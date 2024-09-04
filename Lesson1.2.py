218d7676c8df94e0f66c0dee78ad9cd05de10356
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-09-04 17:18:44 +0300

    04/09/2024

---
+++ Lesson1.2.py
@@ -0,0 +1,6 @@
+completed_home_work=12
+spent_hours=1.5
+course_name='Python'
+time_per_task= spent_hours/completed_home_work
+print (f'Курс: {course_name}, всего задач: {completed_home_work}, затрачено часов: {spent_hours}, среднее время выполнения {time_per_task} часа')
+
