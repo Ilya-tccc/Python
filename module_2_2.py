45cf29a846fd5d88d9d8621820091edfb6f84db9
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-09-10 21:52:33 +0300

    04/09/2024

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+first=input('Введите первое число: ')
+second=input('Введите второе число: ')
+third=input('Введите третье число: ')
+if first==second and first==third:
+    print('Все 3 числа равны')# print(3)
+elif first==second or first==third or second==third:
+    print('2 числа из трех равны')# print(2)
+# elif first!=second and first!=third and second!=third:
+#     print('Все числа не равны')# print(0)
+else:
+    print('Все числа не равны')# print(0)
