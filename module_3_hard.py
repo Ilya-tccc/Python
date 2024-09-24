a4561de4c6f2ea34624a70aa0a6c0c03e5d489bc
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-09-24 16:00:54 +0300

    04/09/2024

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,29 +1,28 @@
-total_sum=0
+total_sum = 0
+
 
 def calculate_structure_sum(*args):
     global total_sum
 
     for i in range(len(args)):
-        if isinstance(args[i],(int,float)):
-            total_sum+=args[i]
-        elif isinstance(args[i],dict):
-            for key,value in args[i].items():
-                calculate_structure_sum(key,value)
-        elif isinstance(args[i],(list,tuple,set)):
+        if isinstance(args[i], (int, float)):
+            total_sum += args[i]
+        elif isinstance(args[i], dict):
+            for key, value in args[i].items():
+                calculate_structure_sum(key, value)  # нужно уточнить этот момент
+        elif isinstance(args[i], (list, tuple, set)):
             calculate_structure_sum(*args[i])
-        elif isinstance(args[i],str):
-            total_sum+=len(args[i])
-    return    total_sum
-
+        elif isinstance(args[i], str):
+            total_sum += len(args[i])
+    return total_sum
 
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 result = calculate_structure_sum(data_structure)
 print(f'Сумма всех чисел и длин элементов равна: {result}')
-
