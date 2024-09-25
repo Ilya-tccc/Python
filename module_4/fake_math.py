c6e44d7676f21b4c4dc6eaec3d0e3a7bde7d7bd9
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-09-25 20:13:29 +0300

    04/09/2024

---
+++ module_4/fake_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def divide(first,second):
+    if second ==0:
+        return f'Ошибка, на ноль делить нельзя'
+    else:
+        return first/second
+
+
+# print(divide(124, 124))#почему данная часть модуля вызывается при импорте функции divide в module4_1?
