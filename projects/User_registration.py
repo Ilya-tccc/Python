7ae8037fe085ac24f1620c7cd038bb7d7e9709ad
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-10-09 16:31:42 +0300

    04/09/2024

--- projects/User_registration.py
+++ projects/User_registration.py
@@ -18,9 +18,9 @@ class User:
 
     def __init__(self, username, password, confirm_password):
         self.username = username
-        self.password = self.check_password(username,password,confirm_password)
-            # if password == confirm_password else print(
-            # 'Введенные пароли отличаются. Повторите регистрацию')
+        self.password = self.check_password(username, password, confirm_password)
+        # if password == confirm_password else print(
+        # 'Введенные пароли отличаются. Повторите регистрацию')
         # if password==confirm_password:
         #     self.password=password
 
@@ -43,7 +43,6 @@ class User:
             password1 = input('Введите пароль ')
             password2 = input('Подтвердите пароль ')
             return self.check_password(user, password1, password2)
-
         else:
             return password1
 
@@ -53,22 +52,22 @@ if __name__ == '__main__':
 
     while True:
         choice = int(input('Приветствую! Выберите действие: \n 1 - Вход \n 2 - Регистрация \n '))
-        if choice==2:
+        if choice == 2:
             user = User(user1 := input('Введите логин '), password1 := input('Введите пароль '),
-                    password2 := input('Подтвердите пароль '))
+                        password2 := input('Подтвердите пароль '))
             # user.password[user1]=user.check_password(user1, password1, password2)
-        # elif len(password1) < 8:
-        #     print("Длинна пароля менее 8 символов. Повторите ввод")
-        #     user = User(f'Логин {user1}', password1 := input('Введите пароль '),
-        #                 password2 := input('Подтвердите пароль '))
-        # elif password1.isnumeric() or password1.islower() or password1.isupper() or password1.isalpha():  # проверки по порядку: только числа, только нижний, только верхний, только буквы
-        #     print(
-        #         "Пароль должен содержать хотя бы один символ, одну цифру и по одной букве в нижнем и верхнем регистре. Повторите ввод!")
+            # elif len(password1) < 8:
+            #     print("Длинна пароля менее 8 символов. Повторите ввод")
+            #     user = User(f'Логин {user1}', password1 := input('Введите пароль '),
+            #                 password2 := input('Подтвердите пароль '))
+            # elif password1.isnumeric() or password1.islower() or password1.isupper() or password1.isalpha():  # проверки по порядку: только числа, только нижний, только верхний, только буквы
+            #     print(
+            #         "Пароль должен содержать хотя бы один символ, одну цифру и по одной букве в нижнем и верхнем регистре. Повторите ввод!")
             database.add_user(user.username, user.password)
-        elif choice==1:
-            user = User(login := input('Введите логин '), password1 := input('Введите пароль '),password1)
+        elif choice == 1:
+            user = User(login := input('Введите логин '), password1 := input('Введите пароль '), password1)
             if login in database:
-                if database.data[login]==password1:
+                if database.data[login] == password1:
                     print(f'привет {login}')
                 else:
                     print("Пароль введен неверно. Повторите ввод")
