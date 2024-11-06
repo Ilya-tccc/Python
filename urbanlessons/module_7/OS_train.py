9e45112a6c3cd7a9803c1121e2410408333931c6
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-11-06 18:37:45 +0300

    04/09/2024

---
+++ urbanlessons/module_7/OS_train.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+directory = os.getcwd()
+
+
+for root, dirs, files in os.walk(directory):
+    # files= [f for f in os.listdir() if os.path.isfile(f)]
+    for file in files:
+        filepath = os.path.join(file)
+        filetime = os.path.getatime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(file)
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
