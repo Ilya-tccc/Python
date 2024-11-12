c0dd0d95a6dcc0535e62049a06a65ffd2f5f6293
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-11-12 14:25:49 +0300

    04/09/2024

--- urbanlessons/module_8/module_8_2.py
+++ urbanlessons/module_8/module_8_2.py
@@ -1,27 +1,28 @@
 def personal_sum(number):
-    res=0
-    incorrect_data=0
+    res = 0
+    incorrect_data = 0
     for n in number:
         try:
-            res+=n
+            res += n
         except TypeError as exc:
-            incorrect_data+=1
-    return (res,incorrect_data)
+            incorrect_data += 1
+    return (res, incorrect_data)
+
+
 def calculate_average(number):
-    res=0
+    res = 0
     try:
-        res=personal_sum(number)[0]/(len(number)-personal_sum(number)[1])
+        res = personal_sum(number)[0] / (len(number) - personal_sum(number)[1])
     except ZeroDivisionError:
-        res=0
+        res = 0
     except TypeError as exc:
         print('В numbers записан некорректный тип данных')
-        res= None
+        res = None
     return res
 
 
-
 #
-print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
