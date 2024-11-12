ebf5c63902a93f5d436f57a2ed3cd30f8f3b9d33
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-11-12 11:57:32 +0300

    04/09/2024

--- urbanlessons/module_7/OS_train.py
+++ urbanlessons/module_7/OS_train.py
@@ -2,15 +2,20 @@ import os
 import time
 
 directory = os.getcwd()
-
+#
+# if os.path.exists('train'):
+#     os.chdir('train')
+# else:
+#     os.mkdir('train')
+#
 
 for root, dirs, files in os.walk(directory):
     # files= [f for f in os.listdir() if os.path.isfile(f)]
     for file in files:
-        filepath = os.path.join(file)
-        filetime = os.path.getatime(file)
+        filepath = os.path.join(root,file)
+        filetime = os.path.getmtime(file)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
+        parent_dir = os.path.dirname(filepath)
         print(
             f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
