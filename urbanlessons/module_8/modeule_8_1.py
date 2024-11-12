3836a2cfb49868fe08b9208326f17f3654e52dd4
Ilya-tccc <ilyaharlanenkov@yandex.ru>
2024-11-12 12:49:20 +0300

    04/09/2024

---
+++ urbanlessons/module_8/modeule_8_1.py
@@ -0,0 +1,14 @@
+def add_everything_up(a,b):
+    try:
+        res=a+b
+    except TypeError as te:
+        res=str(a)+str(b)
+    else:
+        res=round(a+b,2)
+    return res
+
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
